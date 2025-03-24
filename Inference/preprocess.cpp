7fcab687b03f8419ef39edc2ae09144f22995f04
YasmineBenhamadi <yasminebenhamadi5@gmail.com>
2025-03-24 11:46:31 +0100

    updated

--- Inference/preprocess.cpp
+++ Inference/preprocess.cpp
@@ -1,72 +1,21 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
+#include <math.h>
 #include "hdf5.h"
+#include "./bands_preprocess.cpp"
+//#include "./inference.cpp"
 //#include "./butterworth.c"
 
 //g++ -std=c++17 preprocess.cpp -o preprocess -I/opt/homebrew/include -lhdf5
+//g++ -std=c++17 preprocess.cpp -o preprocess -ltensorflowlite -Itensorflow -Ltensorflow/bazel-bin/tensorflow/lite -rpath @executable_path/tensorflow/bazel-bin/tensorflow/lite -I/opt/homebrew/include -lhdf5
 
 
-typedef struct {
-    float xv[11];
-    float yv[11];
-} BW_filter_t;
-
-typedef struct {
-    float gain;
-    float yc[2];
-} BW_filterCoefficients_t;
-
-void Butterworth_initialise(BW_filter_t *filter) {
-    for (uint32_t i = 0; i < 11; i += 1) {
-        filter->xv[i] = 0.0f;
-        filter->yv[i] = 0.0f;
-    }
-}
-
-const std::vector<float> a = {0.24748712,  -1.01459797,   3.25044176,  -6.60480357,
-         11.17397924, -13.96354968,  14.75151685, -11.52428018,
-          7.49511809,  -3.10514426,   1.};
-
-const std::vector<float> b = {-0.00023953,  0.        ,  0.00119764,  0.        , -0.00239528,
-         0.        ,  0.00239528,  0.        , -0.00119764,  0.        ,
-         0.00023953};
-
-
-
-float Butterworth_applyBandPassFilter(float sample, BW_filter_t*filter){
-    //Shift x buffer
-    for (int i=0; i< b.size() - 1; i++){
-        filter->xv[i]=filter->xv[i+1];
-    }
-    filter->xv[b.size() - 1]= sample;
-
-    // sum(x[n-k]*b[k])
-    float feed_fwd=0;
-    for (int i=0; i< b.size(); i++){
-        feed_fwd +=b[i]*filter->xv[i];
-    }
-
-    //Shift y buffer
-    for (int i=0; i< a.size() - 1; i++){
-        filter->yv[i]=filter->yv[i+1];
-    }
-
-    //sum(y[n-k]*a[k])
-    float feed_bck=0;
-    for (int i=0; i< a.size() - 1; i++){
-        feed_bck +=a[i]*filter->yv[i];
-    }
-
-    filter->yv[a.size() - 1] = feed_fwd - feed_bck; 
-    return filter->yv[a.size() - 1]; 
-}
-
-void filter_audio(std::vector<float> audio, std::vector<float> &filtered_audio){
+void filter_audio(std::vector<float> a, std::vector<float> b, std::vector<float> audio, std::vector<float> &filtered_audio){
     BW_filter_t filter_buffer;
     Butterworth_initialise(&filter_buffer);
     for(auto sample:audio){
-        float y_filter = Butterworth_applyBandPassFilter(sample, &filter_buffer);
+        float y_filter = Butterworth_applyBandPassFilter(a,b,sample, &filter_buffer);
         filtered_audio.push_back(y_filter);
     }
 }
@@ -133,25 +82,20 @@ void saveToCSV(const std::vector<float>& vec, const std::string& filename) {
 }
 
 int main(){
-    const char* FILE_NAME = "audio_1.h5";
+    const char* FILE_NAME = "clip_1_20240726_forest_6_5m_0_9_test1_YH_4_D_7.h5f";
     std::vector<float> audio_data; std::vector<float> filtered_audio; std::vector<float> filtered_audio_2;
 
     get_audio(FILE_NAME, audio_data);
 
     std::cout << "here... " << std::endl;
 
-    filter_audio(audio_data, filtered_audio);
+    std::string outfilename = "band1_envelope.csv"; 
 
-    filter_audio(filtered_audio, filtered_audio_2);
-
-    //for (auto y:filtered_audio)
-        //std::cout << y << " " << std::endl;
-
-    std::string outfilename = "filt_audio_cpp.csv"; 
-    saveToCSV(filtered_audio, outfilename);
-
-    std::string outfilename_2 = "filt_audio_2_cpp.csv"; 
-    saveToCSV(filtered_audio_2, outfilename_2);
+    std::string outfilename_2 = "band1.csv"; 
     
+    std::vector<float> band1; std::vector<float> band1_envelope;
+    lfilt_envelope(a_band1, b, audio_data, band1, band1_envelope);
+    saveToCSV(band1_envelope, outfilename);
+    saveToCSV(band1, outfilename_2);    
     return 0;
 }
